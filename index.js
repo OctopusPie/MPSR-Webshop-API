a8e2b052cf0970f3384db0e8abbefe3e383c8f9f
Henri <32584105+OctopusPie@users.noreply.github.com>
2023-03-18 17:27:59 +0100

    Added Env files

--- index.js
+++ index.js
@@ -45,125 +45,16 @@ app.get('/customers/:id', (req, res) => {
     res.status(200).json(customer);
 });
 
-// Login endpoint
-app.post("/login/:email", (req, res) => {
-    const { email } = req.params.email;
-    if (!email) {
-        res.status(404);
-        res.send({
-            message: "You didn't enter a valid email address.",
-        });
+//check if webshop authentification key is correct
+app.post('/customers/auth', (req, res) => {
+    const authKey = req.body.authKey;
+    if (authKey === process.env.AUTH_KEY) {
+        res.status(200).json({message: "Authentification successful", status: 200});
+    }else {
+        res.status(403).json({message: "Authentification failed", status: 403});
     }
-    const token = makeToken(email);
-    const mailOptions = {
-        from: "You Know",
-        html: emailTemplate({
-            email,
-            link: `http://localhost:8090/account?token=${token}`,
-        }),
-        subject: "Your Magic Link",
-        to: email,
-    };
-    return transport.sendMail(mailOptions, (error) => {
-        if (error) {
-            res.status(404);
-            res.send("Can't send email.");
-        } else {
-            res.status(200);
-            res.send(`Magic link sent. : http://localhost:8090/account?token=${token}`);
-        }
-    });
-});
-
-app.get("/account", (req, res) => {
-    isAuthenticated(req, res)
-});
-
-const isAuthenticated = (req, res) => {  const { token } = req.query
-    if (!token) {
-        res.status(403)
-        res.send("Can't verify user.")
-        return
     }
-    let decoded
-    try {
-        decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
-    }
-    catch {
-        res.status(403)
-        res.send("Invalid auth credentials.")
-        return
-    }
-    if (!decoded.hasOwnProperty("email") || !decoded.hasOwnProperty("expirationDate")) {
-        res.status(403)
-        res.send("Invalid auth credentials.")
-        return
-    }
-    const { expirationDate } = decoded
-    if (expirationDate < new Date()) {
-        res.status(403)
-        res.send("Token has expired.")
-        return
-    }
-    res.status(200)
-    res.send("User has been validated.")
-}
-
-
-const transport = nodeMailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: 587,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD
-    }
-});
-
-// Make email template for magic link
-const emailTemplate = ({ username, link }) => `
-  <h2>Bonjour ${username}</h2>
-  <p>Scanner ce QRCode pour vous connectez:</p>
-  <p>${link}</p>`;
-
-// Generate token
-const makeToken = (email) => {
-    const expirationDate = new Date();
-    expirationDate.setHours(new Date().getHours() + 1);
-    return jwt.sign({ email, expirationDate }, process.env.JWT_SECRET_KEY);
-};
-
-
-//create  qr code every time this endpoint is called
-app.get('/qr', (req,res) => {
-    QRCode.toDataURL("https://63d396f0c1ba499e54c3f915.mockapi.io/api/v1/customers", function (err, qrCode) {
-        console.log(qrCode)
-        let transporter = nodemailer.createTransport({
-            service: 'outlook',
-            auth: {
-                user: 'emzil@adress.com',
-                pass: 'password'
-            }
-        });
-
-        let mailOptions = {
-            from: 'henri.spaulding@epsi.fr',
-            to: 'henri.spaulding@epsi.fr',
-            subject: 'Sending Email using Node.js',
-            html: '<img src="'+ qrCode + '" alt="missing qr code">'
-        };
-
-        transporter.sendMail(mailOptions, function(error, info){
-            if (error) {
-                console.log(error);
-            } else {
-                console.log('Email sent: ' + info.response);
-            }
-        });
-    })
-    res.send("qr code sent")
-});
-
-app.delete('/token', (req,res) => {    res.send("destroy token")});
+)
 
 app.listen(process.env.DEV_PORT, () => {  console.log('Server listening on port 8090')});
 
